3ec62eed7404c195795d28b36a345ae9a48fcb99
Mehul Jain <mehul.jain2206@gmail.com>
2026-04-09 22:46:47 +0530

    tabs

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 
 import React from "react";
 import './../styles/App.css';
+import Tabs from "./Tabs"
+
+const posts = [
+  {
+    title: "Welcome",
+    content: "This is the welcome post of our website."
+  },
+  {
+    title: "About Us",
+    content: "We provide high quality tutorials and examples."
+  },
+  {
+    title: "Services",
+    content: "We offer web development and design services."
+  },
+  {
+    title: "Contact",
+    content: "You can reach us via email or phone."
+  },
+  {
+    title: "Blog",
+    content: "Read our latest articles and updates here."
+  }
+];
+
+
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <Tabs data={posts}/>
     </div>
   )
 }
---
+++ src/components/Tabs.js
@@ -0,0 +1,26 @@
+
+import React, { useState } from "react";
+
+
+const Tabs=({data})=>{
+    let[cont,setCont]=useState(data[0])
+    function handleClick(e){
+       let p= e.target.id.slice(4);
+       console.log(p);
+       setCont(data[p]);
+    }
+
+   
+    return(
+        <div>
+            <ul>
+        {    data.map((val,ind)=>
+                <li  id={`list${ind}`}    key={ind} onClick={handleClick}>{val.title}</li>)
+        }
+        </ul>
+        <p>This is the content for {cont.content}</p>
+        </div>
+    )
+
+}
+export default Tabs;
